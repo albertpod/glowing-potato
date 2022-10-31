07540482ecff0401ee0a832da6d79e62872155b9
Albert Podusenko <albert.podusenko@gmail.com>
2022-10-31 13:42:57 +0100

    Update problem2

--- problem2.jl
+++ problem2.jl
@@ -5,46 +5,64 @@ using Flux, ForwardDiff
 
 include("helpers.jl")
 
-function uniform_sum(x, m)
-    aₓ, bₓ = -5.0, 5.0
+function uniform_m(m)
     aₘ, bₘ = 0.0, 3.0
-    x_ = x in aₓ:bₓ ? 1/(b-a) : 0
-    m_ = m in aₘ:bₘ ? 1/(b-a) : 0 
-    return x_ + m_
+    m_ = m in aₘ:bₘ ? 1/(bₘ-aₘ) : 0 
+end
+
+function uniform_x(x)
+    aₓ, bₓ = -5.0, 5.0
+    x_ = x in aₓ:bₓ ? 1/(bₓ-aₓ) : 0
 end
 
 function D(x)
     aₓ, bₓ = -5.0, 5.0
-    x_ = x in aₓ:bₓ ? 1/(b-a) : 0
+    x_ = x in aₓ:bₓ ? 1/(bₓ-aₓ) : 0
     x_ <= 0 ? pdf(NormalMeanVariance(x_ + 1, 0.1), x_) : pdf(NormalMeanVariance(0.0, 100.0), x_)
-end
-
-function constraint(y₁, y₂)
-    return y₁ == y₂ ? 1.0 : 0.0
 end
 
 
 @model [ default_factorisation = MeanField() ] function model2()
-    τsoft = datavar(Float64)
+    τ = datavar(Float64)
 
     α ~ Beta(1.0, 1.0)
-    x ~ NormalMeanVariance(10.0, 10.0)
-    m ~ NormalMeanVariance(-10.0, 10.0)
 
-    xxmm ~ uniform_sum(x, m) where {meta=CVIApproximation(2000, 1000, Descent(1.0))}
+    # workaround for uniform
 
-    d ~ D(x) where {meta=CVIApproximation(2000, 1000, Descent(0.1))}
+    # x ~ NormalMeanVariance(0, 1.0)
+    # m ~ NormalMeanVariance(-4.0, 1.0)
+
+    # x_ ~ uniform_x(x) where {meta=CVIApproximation(2000, 1000, Descent(0.1))}
+    # m_ ~ uniform_m(m) where {meta=CVIApproximation(2000, 1000, Descent(0.1))}
+
+    # x__ ~ NormalMeanVariance(x_, 1.0)
+    # m__ ~ NormalMeanVariance(m_, 1.0)
+
+
+    x__ ~ NormalMeanVariance(0.0, 1.0)
+    m__ ~ NormalMeanVariance(1.0, 1.0)
+
+    x_m ~ x__ + m__ 
+
+    d ~ D(x__) where {meta=CVIApproximation(2000, 1000, Descent(1.0))}
 
     zα ~ Bernoulli(α)
     
-    y  ~ NormalMixture(zα, (d, xxmm), (10000.0, 10))
-    ŷ  ~ NormalMixture(zα, (d, xxmm), (10000.0, 10))
+    y  ~ NormalMixture(zα, (d, x_m), (100.0, 10.0))
+    ŷ  ~ NormalMixture(zα, (d, x_m), (100.0, 10.0))
 
-    τ ~ constraint(y, ŷ) where {meta=CVIApproximation(2000, 1000, Descent(0.1))}
-
-    τsoft ~ NormalMeanVariance(τ, 1e-5)
+    τ ~ NormalMeanVariance(y-ŷ, 1e-4)
 
 end
 
-init_marginals = (xxmm=NormalMeanPrecision(), d = NormalMeanPrecision(), m = NormalMeanPrecision(), x = NormalMeanPrecision(), α  = vague(Beta), τ = NormalMeanPrecision(), ŷ=NormalMeanPrecision(), y=NormalMeanPrecision())
-res = inference(model = Model(model2), data=(τsoft = 1.0,), free_energy=false, initmarginals = init_marginals, initmessages = init_marginals, iterations=10, showprogress=true)
+init_marginals = (x_m=NormalMeanPrecision(), d = NormalMeanPrecision(), m = NormalMeanPrecision(), x = NormalMeanPrecision(), 
+                  m_ = NormalMeanPrecision(), x_ = NormalMeanPrecision(), 
+                  α  = vague(Beta), ŷ=NormalMeanPrecision(), y=NormalMeanPrecision())
+
+res = inference(model = Model(model2), data=(τ = 0.0,), free_energy=false, initmarginals = init_marginals, initmessages = init_marginals, iterations=100, showprogress=true)
+
+res.posteriors[:α][end]
+@. mean(res.posteriors[:x__])[end]
+@. var(res.posteriors[:x__])[end]
+@. mean(res.posteriors[:m__])[end]
+@. var(res.posteriors[:m__])[end]
